95bb7ac8837fdcc9061d71b01483dc986c41066e
hoanganh9610 <33538876+hoanganh9610@users.noreply.github.com>
2024-08-27 20:53:54 +0700

    hihi

---
+++ automationTest/src/automationTest/seleniumIntroduce.java
@@ -0,0 +1,13 @@
+package automationTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+public class seleniumIntroduce {
+    public static void main(String[] args) {
+     System.setProperty("webdriver.chrome.driver", "Users/caothihonghanh/Documents/Chromium/chromedriver");
+     WebDriver driver = new ChromeDriver();
+     
+    }
+}
---
+++ automationTest/src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module automationTest  {
+    requires org.seleniumhq.selenium.chrome_driver;
+    requires org.seleniumhq.selenium.api;
+    requires org.seleniumhq.selenium.remote_driver;
+    requires dev.failsafe;
+    // Add other required modules
+}
